afddb92bbbef570aacf35d5deb0ac13b3e3922fa
hasipersonal <vilochanahasith@gmail.com>
2025-12-21 23:34:44 +0530

    icon

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -15,7 +15,8 @@ const Header = () => {
                     <div className="row">
                         <div className="col-lg-3 col-10">
                             <div className="logo">
-                                <h2><Link to='/home'>Mahela<span><i className="fa fa-heart" aria-hidden="true"></i></span>Nelum</Link></h2>
+                                {/* <h2><Link to='/home'>Mahela<span><i className="fa fa-heart" aria-hidden="true"></i></span>Nelum</Link></h2> */}
+                                <h2><Link to='/home'>Mahela<span><img className='heart-icon' src="../heart-icon.png" alt="Heart Icon" /></span>Nelum</Link></h2>
                             </div>
                         </div>
                         <div className="col-lg-9">
